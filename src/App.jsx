edf48fa91421f3fd13d40ff2631778156eabb0b5
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-27 16:45:23 +0100

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,30 @@ import { tasks } from "../data/tasksData";
 import "./App.css";
 
 function App() {
+  const filteredList = tasks.filter((task) => {
+    if (task.state != "completed") {
+      return task;
+    }
+  });
+
   return (
     <>
       <div>
         <h1>Task Manager</h1>
       </div>
-      <h2>Current Tasks</h2>
+      <div>
+        <h2>Current Tasks</h2>
+        <ul>
+          {filteredList.forEach((task) => {
+            <li>{task.title}</li>;
+          })}
+        </ul>
+      </div>
       <hr />
-      <h2>Current Tasks</h2>
+      <div>
+        <h2>Current Tasks</h2>
+        <ul></ul>
+      </div>
     </>
   );
 }
